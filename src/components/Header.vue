0d20e365b6a756c56d40ff5bff055cdb60fb604a
LucasL <lucas.lofqvist0005@stud.hkr.se>
2026-03-18 11:02:23 +0100

    Added html to header component and removed unecessary css tags

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,21 @@
 <script></script>
 
-<template></template>
+<template>
+  <header>
+    <!--
+            This image has been commented out for now as it will break
+            until you've learnt how to work with images!
+            <img src="logo_vue_300px.png" alt="vue logo" />
+            -->
+    <h1>Hi, I'm <span class="highlight">Vue</span></h1>
+  </header>
+</template>
 
 <style scoped>
-header img {
+img {
   height: 150px;
 }
-header h1 {
+h1 {
   font-size: 2rem;
   color: #34495e;
   margin-top: 10px;
